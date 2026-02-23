6dbf1aac04f22f7b44f47b9db8a3fe158b48de16
Roshan Kumar Khadka <khadkaroshan715@gmail.com>
2026-02-23 16:52:40 +0545

    final commit

--- Bachelor_Sec_A_B/index.js
+++ Bachelor_Sec_A_B/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+const express = require("express");
+const connectToDatabase = require("./db/db");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+connectToDatabase();
+
+app.use("/api/auth", require("./routes/userRoute"));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`)
+});
+
+app.get("/", (req, res) => {
+    res.send("Roshan Kumar Khadka ")
+});
